9754d525d6f1f6661fffcc4db63254b6ca6ca73f
Anatoly Pashin <anatoly.pashin@gmail.com>
2016-02-11 12:34:07 +1000

    chore(schema): doctrine init

---
+++ lib/filter/doctrine/DimensionVersionFormFilter.class.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * DimensionVersion filter form.
+ *
+ * @package    trade
+ * @subpackage filter
+ * @author     Your name here
+ * @version    SVN: $Id: sfDoctrineFormFilterTemplate.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
+ */
+class DimensionVersionFormFilter extends BaseDimensionVersionFormFilter
+{
+  public function configure()
+  {
+  }
+}
---
+++ lib/form/doctrine/GoodVersionForm.class.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * GoodVersion form.
+ *
+ * @package    trade
+ * @subpackage form
+ * @author     Your name here
+ * @version    SVN: $Id: sfDoctrineFormTemplate.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
+ */
+class GoodVersionForm extends BaseGoodVersionForm
+{
+  public function configure()
+  {
+  }
+}
