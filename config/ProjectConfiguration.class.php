ea79c23a228c58e522d3b67ea44f7280d0fbd452
Anatoly Pashin <anatoly.pashin@gmail.com>
2016-03-12 20:33:30 +1000

    feat(order): add mailgun

--- config/ProjectConfiguration.class.php
+++ config/ProjectConfiguration.class.php
@@ -2,9 +2,14 @@
 
 require_once '/usr/share/symfony/lib/autoload/sfCoreAutoload.class.php';
 sfCoreAutoload::register();
+require dirname(__FILE__) . '/../vendor/autoload.php';
 
 class ProjectConfiguration extends sfProjectConfiguration
 {
+  const MAILGUN_KEY = '';
+  const MAILGUN_DOMAIN = '';
+  const MAILGUN_FROM = '';
+
   public function setup()
   {
     $this->enablePlugins('sfDoctrinePlugin');
