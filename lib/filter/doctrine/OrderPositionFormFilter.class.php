0094cc17dd2325eb088d2456ad7226f8374e143a
Anatoly Pashin <anatoly.pashin@gmail.com>
2016-03-12 13:53:39 +1000

    feat(order): model

---
+++ lib/filter/doctrine/OrderPositionFormFilter.class.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * OrderPosition filter form.
+ *
+ * @package    trade
+ * @subpackage filter
+ * @author     Your name here
+ * @version    SVN: $Id: sfDoctrineFormFilterTemplate.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
+ */
+class OrderPositionFormFilter extends BaseOrderPositionFormFilter
+{
+  public function configure()
+  {
+  }
+}
